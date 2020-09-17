db350584de07b8c975bb9efa872a3f91cabd0b39
kanichiki <kansei.ichiki@gmail.com>
2020-09-17 11:23:16 +0900

    clientのcommand変更

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 import * as serviceWorker from './serviceWorker';
+import {Auth0Provider} from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Auth0Provider
+        domain="dev-0hguzgvk.us.auth0.com"
+        clientId="cwgCxkwDG8r5Qga9lX0cYS7if2pJgeLc"
+        redirectUri={window.location.origin}
+    >
+        <App/>
+    </Auth0Provider>,
+    document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
