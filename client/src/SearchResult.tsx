6aa9a77a28fde7401982dad143b43f3110c10cf1
yuto-moriizumi <moriizumi.yuto.18@shizuoka.ac.jp>
2020-09-18 10:51:01 +0900

    検索結果ページを作った /search/検索内容/ でアクセス

--- client/src/SearchResult.tsx
+++ client/src/SearchResult.tsx
@@ -1,48 +1,66 @@
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
 type PageProps = {} & RouteComponentProps<{ query: string }>;
-export default class SearchResult extends React.Component<PageProps> {
-  //const qs = this.props.qs;
+interface PageStates {
+  books: Array<{ name: string; author: string; genre: string; img: string }>;
+}
+export default class SearchResult extends React.Component<
+  PageProps,
+  PageStates
+> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      books: [
+        {
+          name: "小説　盗作",
+          author: "やば太郎",
+          genre: "小説",
+          img:
+            "https://images-na.ssl-images-amazon.com/images/I/51XXinn9iFL._SX258_BO1,204,203,200_.jpg",
+        },
+      ],
+    };
+
+    //後でAPI呼び出しに置き換えます
+    setTimeout(() => {
+      const books = this.state.books;
+      books.push({
+        name: "やば太郎大冒険",
+        author: "やば太郎",
+        genre: "小説",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/51XXinn9iFL._SX258_BO1,204,203,200_.jpg",
+      });
+      this.setState({ books: books });
+    }, 1000);
+  }
 
   render() {
     return (
-      <div>
-        <header>
-          <p>ヘッダコンポーネントがここに入ります</p>
-          <h1>Search Result</h1>
-          <input type="text" name="検索バー" id="" />
-          <input type="submit" value="検索" />
-        </header>
-        <p>「{this.props.match.params.query}」の検索結果</p>
-        <ul>
-          <li></li>
-          <li></li>
-        </ul>
-        <ul>
-          <li>
-            <img src="" alt="" />
-            <div>
-              <p>小説　盗作</p>
-              <p>著者　やば太郎</p>
-              <p>ジャンル　小説</p>
-            </div>
-          </li>
-        </ul>
-        <table>
-          <tr>
-            <th>本のタイトル</th>
-            <th>写真</th>
-          </tr>
-          <tr>
-            <td>小説　盗作</td>
-            <td>写真</td>
-          </tr>
-          <tr>
-            <td>小説　やば日記</td>
-            <td>写真</td>
-          </tr>
-        </table>
+      <div className="container-fluid">
+        <h1>「{this.props.match.params.query}」の検索結果</h1>
+        <ul className="list-group">{this.renderList()}</ul>
       </div>
     );
+  }
+
+  //配列からliノードのリストを返します
+  private renderList() {
+    const rows = this.state.books.map((book, index) => (
+      <li className="list-group-item" key={index}>
+        <div className="row">
+          <img src={book.img} alt="" className="col-xs-6" />
+          <div className="col-xs-6">
+            <p>{book.name}</p>
+            <p>著者　{book.author}</p>
+            <p>ジャンル　{book.genre}</p>
+          </div>
+        </div>
+      </li>
+    ));
+
+    return rows;
   }
 }
