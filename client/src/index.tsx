dcb29bbadb44781b0b200212dd8287901fb39404
yuto-moriizumi <moriizumi.yuto.18@shizuoka.ac.jp>
2020-09-18 14:11:22 +0900

    コンポーネントを正しく再配置しました

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
-import SearchResult from "./SearchResult";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "@auth0/auth0-react";
 
