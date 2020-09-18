cbc1cf181b31fbe82bb9b3f258c97f0c289189e6
ThunderRa1n <Podsn0w@gmail.com>
2020-09-18 14:44:20 +0900

    restapi sample

--- server/app.py
+++ server/app.py
@@ -1,4 +1,24 @@
-from api import app
+from flask import Flask, jsonify
 
-if __name__ == '__main__':
-  app.run()
+from flask_restful import Api
+
+from database import init_db
+
+from apis.hoge import HogeListAPI, HogeAPI
+
+
+def create_app():
+
+  app = Flask(__name__)
+  app.config.from_object('config.Config')
+
+  init_db(app)
+
+  api = Api(app)
+  api.add_resource(HogeListAPI, '/hoges')
+  api.add_resource(HogeAPI, '/hoges/<id>')
+
+  return app
+
+
+app = create_app()
--- server/config.py
+++ server/config.py
@@ -1,12 +1,15 @@
-class SystemConfig:
+class DevelopmentConfig:
 
-  DEBUG = True
-
-  SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+  # SQLAlchemy
+  SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8'.format(
+    **{
       'user': 'root',
       'password': 'hoge',
       'host': 'db',
-      'db_name': 'hoge'
-  })
+      'database': 'hoge',
+    })
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SQLALCHEMY_ECHO = False
 
-Config = SystemConfig
+
+Config = DevelopmentConfig
