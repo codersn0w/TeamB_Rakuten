a176a780a52c65b759cf9efe9e4906d045966f79
koudai <61653253+koudai0619@users.noreply.github.com>
2020-09-20 19:31:13 +0900

    crud実装

--- server/models/genre.py
+++ server/models/genre.py
@@ -1,37 +1,32 @@
 from datetime import datetime
-
 from flask_marshmallow import Marshmallow
-
 from flask_marshmallow.fields import fields
-
-from sqlalchemy_utils import UUIDType
-
 from database import db
 
 ma = Marshmallow()
 
 
 class GenreModel(db.Model):
-  __tablename__ = 'genres'
-  __table_args__ = {'extend_existing': True}
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(255), nullable=False)
+    __tablename__ = 'genres'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    createTime = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updateTime = db.Column(db.DateTime, nullable=False,
+                           default=datetime.now, onupdate=datetime.now)
 
-  createTime = db.Column(db.DateTime, nullable=False, default=datetime.now)
-  updateTime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+    def __init__(self, name):
+        # self.id = id
+        self.name = name
 
-  def __init__(self, id, name):
-    self.id = id
-    self.name = name
-
-
-  def __repr__(self):
-    return '<GenreModel {}:{}>'.format(self.id, self.name)
+    def __repr__(self):
+        return '<GenreModel {}:{}>'.format(self.id, self.name)
 
 
 class GenreSchema(ma.Schema):
-  class Meta:
-    model = GenreModel
+    class Meta:
+     # 欲しいデータを記述
+        fields = ("id", "name", "createTime", "updateTime")
 
-  createTime = fields.DateTime('%Y-%m-%dT%H:%M:%S')
-  updateTime = fields.DateTime('%Y-%m-%dT%H:%M:%S')
+    createTime = fields.DateTime('%Y-%m-%dT%H:%M:%S')
+    updateTime = fields.DateTime('%Y-%m-%dT%H:%M:%S')
