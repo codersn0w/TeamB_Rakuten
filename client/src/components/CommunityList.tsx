d1dc852aea47a9d221e637cea5d92f17bb65309c
kanichiki <kansei.ichiki@gmail.com>
2020-09-21 12:11:29 +0900

    トップページのコミュニティ一覧表示部分をバックエンド連携

--- client/src/components/CommunityList.tsx
+++ client/src/components/CommunityList.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {Community} from "./Community";
 import {Grid} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import axios from "axios";
 
 type CommunityType = Readonly<{ name: string }>
 const initialObject: CommunityType = {
@@ -19,12 +20,9 @@ export const CommunityList = () => {
     const [communities, setCommunities] = useState([initialObject])
     useEffect(() => {
         const fetchData = async () => {
-            setCommunities([
-                {name: "ミステリー"},
-                {name: "哲学"},
-                {name: "プログラミング"},
-                {name: "数学"},
-            ])
+            const res = await axios.get("http://localhost:5000/genres");
+            console.log(res);
+            setCommunities(res.data.items);
         }
         fetchData()
     }, [])
@@ -35,7 +33,7 @@ export const CommunityList = () => {
                 <Grid item xs={1}></Grid>
                 <Grid item xs={10}>
                     <Grid container>
-                    {communities.map((community, index) => (
+                    {communities && communities.map((community, index) => (
                         <Grid item xs={4} key={index}>
                             <Grid container justify="center">
                                 <Community name={community.name}></Community>
