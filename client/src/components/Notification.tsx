4edf36b8963df523050265550a78b2b7b751d3e1
yuto-moriizumi <moriizumi.yuto.18@shizuoka.ac.jp>
2020-09-23 14:29:36 +0900

    通知一覧ページからリクエスト詳細ページへの遷移を追加

--- client/src/components/Notification.tsx
+++ client/src/components/Notification.tsx
@@ -3,7 +3,13 @@ import { RouteComponentProps } from "react-router-dom";
 
 type Props = {} & RouteComponentProps<{ id: string }>;
 interface States {
-  items: Array<{ img: string; desc: string; date: string }>;
+  items: Array<{
+    img: string;
+    desc: string;
+    date: string;
+    type: string; //通知の種類 レンタルリクエストの承認の場合はリクエストエンティティのIDをidに指定
+    id: number;
+  }>;
 }
 export default class NotificationComponent extends React.Component<
   Props,
@@ -25,12 +31,16 @@ export default class NotificationComponent extends React.Component<
               "https://images-na.ssl-images-amazon.com/images/I/51XXinn9iFL._SX258_BO1,204,203,200_.jpg",
             desc: "鈴木さんが、「小説 疾風…」の貸出をリクエストしています",
             date: "2020/10/10",
+            type: "request",
+            id: 1,
           },
           {
             img:
               "https://images-na.ssl-images-amazon.com/images/I/51XXinn9iFL._SX258_BO1,204,203,200_.jpg",
             desc: "鈴木さんが、「盗作」の貸出をリクエストしています",
             date: "2020/10/09",
+            type: "request",
+            id: 2,
           },
         ],
       });
@@ -43,11 +53,11 @@ export default class NotificationComponent extends React.Component<
         <h1>通知</h1>
         {this.state.items.map((item, index) => (
           <li className="list-group-item" key={index}>
-            <div className="row">
+            <a className="row" href={"request/view/" + item.id}>
               <img src={item.img} alt="" className="col-xs-1" />
               <p className="col-xs-10">{item.desc}</p>
               <p className="col-xs-3">{item.date}</p>
-            </div>
+            </a>
           </li>
         ))}
       </main>
