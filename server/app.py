36b9a5f7f1add2e02e0027d82659077a563d8365
koudai <61653253+koudai0619@users.noreply.github.com>
2020-09-23 14:02:50 +0900

    rentalbooksのcrud実装

--- server/app.py
+++ server/app.py
@@ -1,25 +1,34 @@
 from flask import Flask, jsonify
-
 from flask_restful import Api
+from .database import init_db
+#from apis.hoge import HogeListAPI, HogeAPI
+from .apis.genre import GenreListAPI, GenreAPI
+from .apis.thread import ThreadListAPI, ThreadAPI
+from .apis.message import MessageListAPI, MessageAPI
+from .apis.bookrental import BookRentalListAPI, BookRentalAPI
+from .config import Config
 
-from database import init_db
-
-from apis.hoge import HogeListAPI, HogeAPI
-from apis.genre import GenreAPI
 
 def create_app():
 
-  app = Flask(__name__)
-  app.config.from_object('config.Config')
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-  init_db(app)
+    init_db(app)
 
-  api = Api(app)
-  api.add_resource(HogeListAPI, '/hoges')
-  api.add_resource(HogeAPI, '/hoges/<id>')
-  api.add_resource(GenreAPI, '/community/<id>')
+    api = Api(app)
+    #api.add_resource(HogeListAPI, '/hoges')
+    #api.add_resource(HogeAPI, '/hoges/<id>')
+    api.add_resource(GenreListAPI, '/genres')
+    api.add_resource(GenreAPI, '/genres/<id>')
+    api.add_resource(ThreadListAPI, '/threads')
+    api.add_resource(ThreadAPI, '/threads/<id>')
+    api.add_resource(MessageListAPI, '/messages')
+    api.add_resource(MessageAPI, '/messages/<id>')
+    api.add_resource(BookRentalListAPI, '/bookrentals')
+    api.add_resource(BookRentalAPI, '/bookrentals/<id>')
 
-  return app
+    return app
 
 
 app = create_app()
