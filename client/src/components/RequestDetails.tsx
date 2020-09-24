0a99cae2120bb7a8a98cc9e321a5647263125712
yuto-moriizumi <moriizumi.yuto.18@shizuoka.ac.jp>
2020-09-24 11:07:31 +0900

    RequestDetailsをhookAPIに書き換え

--- client/src/components/RequestDetails.tsx
+++ client/src/components/RequestDetails.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 
 type Props = {} & RouteComponentProps<{ id: string }>;
@@ -9,61 +9,68 @@ interface States {
   kikan: number;
   date: string;
 }
-export default class RequestDetails extends React.Component<Props, States> {
-  constructor(props: any) {
-    super(props);
+export const RequestDetails = () => {
+  //   this.state = {
+  //     img: "",
+  //     name: "",
+  //     requester: "string;",
+  //     kikan: 1,
+  //     date: "string;",
+  // };
 
-    this.state = {
-      img: "",
-      name: "",
-      requester: "string;",
-      kikan: 1,
-      date: "string;",
-    };
+  const [img, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [requester, setRequester] = useState("");
+  const [date, setDate] = useState("");
+  const [kikan, setKikan] = useState(1);
 
-    //後でAPI呼び出しに置き換えます
-    setTimeout(() => {
-      this.setState({
-        img:
-          "https://images-na.ssl-images-amazon.com/images/I/51XXinn9iFL._SX258_BO1,204,203,200_.jpg",
-        name: "インターン伝説III",
-        requester: "森泉友登",
-        kikan: 10,
-        date: "2020/09/18",
-      });
-    }, 100);
-  }
+  //後でAPI呼び出しに置き換えます
+  setTimeout(() => {
+    setImage(
+      "https://images-na.ssl-images-amazon.com/images/I/51XXinn9iFL._SX258_BO1,204,203,200_.jpg"
+    );
+    setName("インターン伝説III");
+    setRequester("森泉友登");
+    setKikan(10);
+    setDate("2020/09/18");
+    // this.setState({
+    //   img:
+    //     "https://images-na.ssl-images-amazon.com/images/I/51XXinn9iFL._SX258_BO1,204,203,200_.jpg",
+    //   name: "インターン伝説III",
+    //   requester: "森泉友登",
+    //   kikan: 10,
+    //   date: "2020/09/18",
+    // });
+  }, 100);
 
-  private accept() {
+  const accept = () => {
     //ここでリクエストAPIを呼び出す
     alert("リクエストを承認しました");
-  }
-  private decline() {
+  };
+  const decline = () => {
     //ここでリクエストAPIを呼び出す
     alert("リクエストを拒否しました");
-  }
+  };
 
-  render() {
-    return (
-      <main className="container-fluid">
-        <h1>リクエスト詳細</h1>
-        <div className="row">
-          <img src={this.state.img} alt="" className="col-xs-6" />
-          <div className="col-xs-6">
-            <p>リクエストした人 {this.state.requester}</p>
-            <p>借りたい日数 {this.state.kikan}</p>
-            <p>リクエスト日 {this.state.date}</p>
-          </div>
+  return (
+    <main className="container-fluid">
+      <h1>リクエスト詳細</h1>
+      <div className="row">
+        <img src={img} alt="" className="col-xs-6" />
+        <div className="col-xs-6">
+          <p>リクエストした人 {requester}</p>
+          <p>借りたい日数 {kikan}</p>
+          <p>リクエスト日 {date}</p>
         </div>
-        <div className="row">
-          <button onClick={this.accept} className="btn btn-primary btn-lg">
-            承認
-          </button>
-          <button onClick={this.decline} className="btn btn-danger btn-lg">
-            拒否
-          </button>
-        </div>
-      </main>
-    );
-  }
-}
+      </div>
+      <div className="row">
+        <button onClick={accept} className="btn btn-primary btn-lg">
+          承認
+        </button>
+        <button onClick={decline} className="btn btn-danger btn-lg">
+          拒否
+        </button>
+      </div>
+    </main>
+  );
+};
